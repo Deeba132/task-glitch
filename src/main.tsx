59b35438b2274ae78eb27455590bbb8f07597a40
Deeba132 <deebashabreen@gmail.com>
2026-01-08 11:55:54 +0530

    removed React.strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import App from './App';
-import { appTheme } from './theme';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import App from "./App";
+import { appTheme } from "./theme";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ThemeProvider theme={appTheme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
 );
-
-
